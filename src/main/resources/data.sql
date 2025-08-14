70bb06c54f32ef97700b9e9e55cd53bf183b7058
Yashas Chandra <yashasc98@gmail.com>
2025-08-14 07:19:50 +0000

    data.sql auto run on mysql server

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,4 @@
 TRUNCATE TABLE station;
-
 INSERT INTO station (id, city, name, line, active) VALUES (1, 'Bangalore', 'Majestic', 'purple', 'Y');
 INSERT INTO station (id, city, name, line, active) VALUES (2, 'Bangalore', 'MG Road', 'purple', 'Y');
 INSERT INTO station (id, city, name, line, active) VALUES (3, 'Bangalore', 'Rajajinagar', 'green', 'Y');
@@ -7,5 +6,3 @@ INSERT INTO station (id, city, name, line, active) VALUES (4, 'Bangalore', 'Yesh
 INSERT INTO station (id, city, name, line, active) VALUES (5, 'Bangalore', 'Indiranagar', 'purple', 'Y');
 INSERT INTO station (id, city, name, line, active) VALUES (6, 'Bangalore', 'Jayanagar', 'green', 'Y');
 INSERT INTO station (id, city, name, line, active) VALUES (7, 'Bangalore', 'Peenya Industry', 'green', 'Y');
-
-COMMIT;
